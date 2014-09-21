833d047605db07cd62ca2570387cc63efd942e60
hexavis <rich.jac@gmail.com>
2014-09-20 20:17:28 -0400

    Added more pages and content

--- Portfolio/My Project/Resources.Designer.vb
+++ Portfolio/My Project/Resources.Designer.vb
@@ -63,9 +63,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property logo() As System.Drawing.Bitmap
+        Friend ReadOnly Property logoreal() As System.Drawing.Bitmap
             Get
-                Dim obj As Object = ResourceManager.GetObject("logo", resourceCulture)
+                Dim obj As Object = ResourceManager.GetObject("logoreal", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
